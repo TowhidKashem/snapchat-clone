4092bf09bf8a0122c959da4466eb4d17a1551d6e
TowhidKashem <towhid.kashem@bit.ly>
2020-04-27 23:00:51 -0400

    Footer

--- src/pages/Layout/Footer/index.tsx
+++ src/pages/Layout/Footer/index.tsx
@@ -1,6 +1,35 @@
-import React from "react";
-import styles from "./index.module.scss";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCommentAlt,
+  faMobile,
+  faMobileAlt,
+  faCircle,
+  faLaugh
+} from '@fortawesome/free-solid-svg-icons';
+import styles from './index.module.scss';
 
-const Footer = () => <footer className={styles.footer}>footer</footer>;
+const Footer = () => (
+  <footer className={styles.footer}>
+    <section className={styles.top}>
+      <FontAwesomeIcon icon={faCircle} className={styles.capture} />
+      <FontAwesomeIcon icon={faLaugh} className={styles.filter} />
+    </section>
+    <section className={styles.bottom}>
+      <article>
+        <FontAwesomeIcon icon={faCommentAlt} />
+        Chat
+      </article>
+      <article>
+        <FontAwesomeIcon icon={faMobile} />
+        <FontAwesomeIcon icon={faMobileAlt} />
+      </article>
+      <article>
+        <FontAwesomeIcon icon={faMobile} />
+        Discover
+      </article>
+    </section>
+  </footer>
+);
 
 export default Footer;
