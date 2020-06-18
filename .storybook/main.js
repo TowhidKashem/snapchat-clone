0b9118c3b48257ae1bac798a3d3408371f082341
TowhidKashem <towhid.kashem@bit.ly>
2020-06-18 13:51:01 -0400

    Moved stories into component folders

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
+  stories: ['../src/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-knobs/register'],
 };
