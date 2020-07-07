bfadd23a1d92d4360c5d8032cf598c1a47ef9f09
TowhidKashem <towhid.kashem@bit.ly>
2020-07-07 01:14:13 -0400

    Moved some things around in tests

--- cypress/integration/camera.spec.ts
+++ cypress/integration/camera.spec.ts
@@ -1,19 +1,42 @@
 /// <reference types="cypress" />
 
-context('Photo Capture', () => {
-  beforeEach(() => {
-    cy.loadApp();
-    cy.get('[data-test=btn-capture]').as('captureBtn');
-    cy.get('@captureBtn').click();
-    cy.get('[data-test=photo-capture]').as('photoCapture');
+describe('Camera', () => {
+  describe('Filters', () => {
+    beforeEach(() => {
+      cy.loadApp();
+      cy.get('[data-test=btn-filters]').as('filtersBtn');
+      cy.get('[data-test=filter-canvas]').as('filterCanvas');
+      cy.get('@filtersBtn').click();
+    });
+
+    it('can see filter buttons', () => {
+      cy.get('[data-test=filters]').should('be.visible');
+    });
+
+    it('filter is set', () => {
+      cy.wait(500);
+      // Jeelizfacefilter sets the width and height attributes on the canvas
+      // if a filter is successfully set so thats one way to tell if the filter worked
+      cy.get('@filterCanvas').and('have.attr', 'width');
+      cy.get('@filterCanvas').and('have.attr', 'height');
+    });
   });
 
-  it('can take photo', () => {
-    cy.get('@photoCapture').should('be.visible');
-  });
+  describe('Photo Capture', () => {
+    beforeEach(() => {
+      cy.loadApp();
+      cy.get('[data-test=btn-capture]').as('captureBtn');
+      cy.get('@captureBtn').click();
+      cy.get('[data-test=photo-capture]').as('photoCapture');
+    });
 
-  it('can close out of photo', () => {
-    cy.get('@photoCapture').find('[data-test=close-btn]').click();
-    cy.get('@photoCapture').should('not.be.visible');
+    it('can take photo', () => {
+      cy.get('@photoCapture').should('be.visible');
+    });
+
+    it('can close out of photo', () => {
+      cy.get('@photoCapture').find('[data-test=close-btn]').click();
+      cy.get('@photoCapture').should('not.be.visible');
+    });
   });
 });
