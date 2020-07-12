51308f1df6cc874becd86a5b62261144d7781ca4
TowhidKashem <towhid.kashem@bit.ly>
2020-07-12 15:59:14 -0400

    Updates

--- precache-manifest.8118fc2e2e996baa545b7176f4240568.js
+++ precache-manifest.8118fc2e2e996baa545b7176f4240568.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d0c2f819b7932b3304cd75feeff1aba9",
+    "revision": "e8c693a62bbfd5bcb26d4b1bfc2e6790",
     "url": "/snapchat-clone/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/snapchat-clone/static/css/2.dda6c3ea.chunk.css"
   },
   {
-    "revision": "7c8f15cf95474cd7fe1c",
+    "revision": "01750d38a1eaf8f0108d",
     "url": "/snapchat-clone/static/css/main.e67b333c.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/snapchat-clone/static/js/2.5ff46a2d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7c8f15cf95474cd7fe1c",
-    "url": "/snapchat-clone/static/js/main.1d9f820e.chunk.js"
+    "revision": "01750d38a1eaf8f0108d",
+    "url": "/snapchat-clone/static/js/main.ff05b6f4.chunk.js"
   },
   {
     "revision": "3630896dcc846c07d505",
